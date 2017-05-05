e692ff35175379bee4e07b1905d4118c7886166f
Charlie DiGiovanna <cd17822@gmail.com>
2017-05-04 22:22:46 -0400

    portfolio vc set up

---
+++ StockVersus/StockVersus/StockRowsView.swift
@@ -0,0 +1,21 @@
+//
+//  StockRowsView.swift
+//  StockVersus
+//
+//  Created by Charlie DiGiovanna on 5/4/17.
+//  Copyright © 2017 Charlie DiGiovanna. All rights reserved.
+//
+
+import UIKit
+
+class StockRowsView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ StockVersus/StockVersus/StockRowsViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  StockRowsViewViewCell.swift
+//  StockVersus
+//
+//  Created by Charlie DiGiovanna on 5/4/17.
+//  Copyright © 2017 Charlie DiGiovanna. All rights reserved.
+//
+
+import UIKit
+
+class StockRowsViewViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

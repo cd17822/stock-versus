5d05d815d5d7a33f1ecbeb34875536e984a123b0
Charlie DiGiovanna <cd17822@gmail.com>
2017-05-11 12:03:57 -0400

    one last thing

--- StockVersus/StockVersus/NewOrderView.swift
+++ StockVersus/StockVersus/NewOrderView.swift
@@ -80,7 +80,9 @@ class NewOrderView: UIView {
             if self.shares_field.text != nil && self.shares_field.text! == "0"
             {
                 self.shares_field.text = ""
+                return
             }
+            
             self.individual_cost_label.text = self.ticker_price!.dollarString
 
             self.total_cost_label.text = (self.ticker_price! * Float(self.shares_field.text!)!).dollarString
